38774068292e8ee6f22ea22ec80417ba113ded9c
Albert Chung <albertswchung@gmail.com>
2025-04-03 21:58:19 +0100

    feat: add residual connections.

--- qm9_gnn/models/gnn.py
+++ qm9_gnn/models/gnn.py
@@ -13,12 +13,14 @@ class MolGNN(nn.Module):
         out_channels: int,
         num_layers: int,
         mp_layer: MessagePassing,
+        residual_every: int = 2,
     ):
         super().__init__()
         self.in_channels = in_channels
         self.hidden_channels = hidden_channels
         self.out_channels = out_channels
         self.num_layers = num_layers
+        self.residual_every = residual_every
 
         self.layers = nn.ModuleList(
             mp_layer(
@@ -35,10 +37,16 @@ class MolGNN(nn.Module):
 
     def forward(self, data: Batch):
         x, edge_index = data.x, data.edge_index
-        for norm, layer in zip(self.norms, self.layers):
+
+        residual = None
+        for idx, (norm, layer) in enumerate(zip(self.norms, self.layers)):
             x = norm(x)
             x = layer(x, edge_index)
             x = F.relu(x)
+            if idx % self.residual_every == 0:
+                if residual is not None:
+                    x = x + residual
+                residual = x
 
         x = scatter(x, data.batch, dim=0, reduce="mean")
         x = self.linear(x)
